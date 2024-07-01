b785aa891c917c5e759e8608536136fc8d3ff474
imagecaptioningmulti <imagecaptioningmulti@gmail.com>
2024-07-01 15:20:19 +0530

    updated

--- app.py
+++ app.py
@@ -1,82 +1,82 @@
 from flask import Flask, request, jsonify, render_template, url_for
 from flask_cors import CORS
-import os
-import io
+import tensorflow as tf
+import numpy as np
 from PIL import Image
-import torch
-from torchvision import transforms
-from data_loader import get_loader
-from model import DecoderRNN, EncoderCNN
-from nlp_utils import clean_sentence
-
+import pickle
+import io
+import os
 app = Flask(__name__)
 # Configuration
 
 
-cocoapi_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 CORS(app)
-# Defining a transform to pre-process the testing images.
-transform_test = transforms.Compose(
-    [
-        transforms.Resize(256),  # smaller edge of image resized to 256
-        transforms.CenterCrop(224),  # get 224x224 crop from center
-        transforms.ToTensor(),  # convert the PIL Image to a tensor
-        transforms.Normalize(
-            (0.485, 0.456, 0.406),  # normalize image for pre-trained model
-            (0.229, 0.224, 0.225),
-        ),
-    ]
-)
-# Creating the data loader.
-data_loader = get_loader(transform=transform_test, mode="test", cocoapi_loc=cocoapi_dir)
 
-device = torch.device("cpu")
 
-# Specify the saved models to load.
-encoder_file = "encoder-3.pkl"
-decoder_file = "decoder-3.pkl"
+# Load the encoder model
+encoder_model_path = './FinalModels/encoder_model'
+loaded_encoder = tf.keras.models.load_model(encoder_model_path)
 
-# Select appropriate values for the Python variables below.
-embed_size = 256
-hidden_size = 512
+# Load the decoder model
+decoder_model_path = './FinalModels/decoder_model'
+loaded_decoder = tf.keras.models.load_model(decoder_model_path)
 
-# The size of the vocabulary.
-vocab_size = len(data_loader.dataset.vocab)
+# Load the tokenizer
+with open('./FinalModels/tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
 
-# Initialize the encoder and decoder, and set each to inference mode.
-encoder = EncoderCNN(embed_size)
-decoder = DecoderRNN(embed_size, hidden_size, vocab_size)
-encoder.eval()
-decoder.eval()
+max_length = 35
+attention_features_shape = 64
 
-# Load the trained weights.
-encoder.load_state_dict(torch.load(os.path.join("./captioning_models", encoder_file), map_location=device))
-decoder.load_state_dict(torch.load(os.path.join("./captioning_models", decoder_file), map_location=device))
+def load_image(image_path):    
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)    
+    return img, image_path
 
-# Move models to CPU
-encoder.to(device)
-decoder.to(device)
+# Image feature extraction model
+image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+new_input = image_model.input 
+hidden_layer = image_model.layers[-1].output 
+image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
 
-def predict_caption(image):
-    if image is None:
-        return "Please select an image"
+def evaluate_V1(image, loaded_encoder, loaded_decoder, max_length, tokenizer, attention_features_shape):
+    attention_plot = np.zeros((max_length, attention_features_shape))
 
-    image = transform_test(image).unsqueeze(0)
-    with torch.no_grad():
-        # Moving image Pytorch Tensor to CPU
-        image = image.to(device)
+    # Manually initialize the hidden state for the Decoder
+    hidden = tf.zeros((1, loaded_decoder.layers[2].units))  # Adjust units based on your GRU layer
 
-        # Obtaining the embedded image features.
-        features = encoder(image).unsqueeze(1)
+    temp_input = tf.expand_dims(load_image(image)[0], 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
 
-        # Passing the embedded image features through the model to get a predicted caption.
-        output = decoder.sample(features)
+    features = loaded_encoder(img_tensor_val)
 
-    sentence = clean_sentence(output, data_loader.dataset.vocab.idx2word)
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
 
-    return sentence
+    for i in range(max_length):
+        predictions, hidden, attention_weights = loaded_decoder(dec_input, features, hidden)
+
+        attention_plot[i] = tf.reshape(attention_weights, (-1,)).numpy()
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result, attention_plot, predictions
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    attention_plot = attention_plot[:len(result), :]
+    return result, attention_plot, predictions
+
+
+
+      
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -86,27 +86,14 @@ def predictweb():
     '''
     For rendering results on HTML GUI
     '''
-    # name = request.form['name']
-    if 'image' not in request.files:
-        return jsonify({"error": "No image uploaded"}), 400
-    
     file = request.files['image']
-  
-    # filename = os.path.join('./uploads', file.filename)
-    # file.save(filename)
-    if file and file.filename != '':
-        img = Image.open(io.BytesIO(file.read())).convert("RGB")
-        caption = predict_caption(img)
-        image_url = url_for('static', filename='uploads/' + file.filename)
-        
-
-    # Save the uploaded image to a static/uploads directory for display
-        file.save(os.path.join('static/uploads', file.filename))
-        return render_template('index.html', prediction_text=':{}'.format(caption),image_url=image_url)
+    filename = os.path.join('./uploads', file.filename)
+    file.save(filename)
+    result, attention_plot, predictions = evaluate_V1(filename, loaded_encoder, loaded_decoder, max_length=20, tokenizer=tokenizer, attention_features_shape=64)
+    pred_caption = ' '.join(result)   
     
-    else:
-        return jsonify({"error": "Invalid image"}), 400
-    # return render_template('index.html', prediction_text='Hello, {}'.format(name))
+    return render_template('index.html', prediction_text=':{}'.format(pred_caption))
+   
 
 @app.route('/get_example', methods=['GET'])
 def get_example():
@@ -124,19 +111,11 @@ def post_example():
     return jsonify(message="This is an example POST response", received_data=data)
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'image' not in request.files:
-        return jsonify({"error": "No image uploaded"}), 400
-    
-    file = request.files['image']
-    print(file)
-    # filename = os.path.join('./uploads', file.filename)
-    # file.save(filename)
-    if file and file.filename != '':
-        img = Image.open(io.BytesIO(file.read())).convert("RGB")
-        caption = predict_caption(img)
-        return jsonify({"caption": caption})
-    
-    else:
-        return jsonify({"error": "Invalid image"}), 400
+    file = request.files['file']
+    filename = os.path.join('./uploads', file.filename)
+    file.save(filename)
+    result, attention_plot, predictions = evaluate_V1(filename, loaded_encoder, loaded_decoder, max_length=20, tokenizer=tokenizer, attention_features_shape=64)
+    pred_caption = ' '.join(result)   
+    return jsonify({'caption': ' '.join(result)})
 if __name__ == "__main__":
     app.run(debug=True)

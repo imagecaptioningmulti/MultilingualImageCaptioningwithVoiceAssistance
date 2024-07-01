2b067857ef421f23e84a87fefd5d000a48622268
imagecaptioningmulti <imagecaptioningmulti@gmail.com>
2024-07-01 15:54:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def predict():
     pred_caption = ' '.join(result)   
     return jsonify({'caption': ' '.join(result)})
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)

96a50d09dcc3888c25c60a0578702765fc1ee56f
imagecaptioningmulti <imagecaptioningmulti@gmail.com>
2024-07-02 08:46:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ app = Flask(__name__)
 CORS(app)
 
 
-# Load the encoder model
-encoder_model_path = './FinalModels/encoder_model'
-loaded_encoder = tf.keras.models.load_model(encoder_model_path)
 
-# Load the decoder model
-decoder_model_path = './FinalModels/decoder_model'
-loaded_decoder = tf.keras.models.load_model(decoder_model_path)
+def load_models():
+    loaded_encoder = tf.keras.models.load_model(encoder_model_path)
+    loaded_decoder = tf.keras.models.load_model(decoder_model_path)
 
-# Load the tokenizer
-with open('./FinalModels/tokenizer.pkl', 'rb') as f:
-    tokenizer = pickle.load(f)
+    # Manually compile the models if required
+    optimizer = tf.keras.optimizers.Adam()
+    loaded_encoder.compile(optimizer=optimizer)
+    loaded_decoder.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy')  # Replace with your actual loss function
 
-max_length = 35
-attention_features_shape = 64
+    return loaded_encoder, loaded_decoder
+
+
+
 
 def load_image(image_path):    
     img = tf.io.read_file(image_path)
@@ -42,7 +42,9 @@ image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imag
 new_input = image_model.input 
 hidden_layer = image_model.layers[-1].output 
 image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
-
+# Load the pre-trained encoder and decoder models
+encoder_model_path = './FinalModels/encoder_model'
+decoder_model_path = './FinalModels/decoder_model'
 def evaluate_V1(image, loaded_encoder, loaded_decoder, max_length, tokenizer, attention_features_shape):
     attention_plot = np.zeros((max_length, attention_features_shape))
 
@@ -81,41 +83,26 @@ def evaluate_V1(image, loaded_encoder, loaded_decoder, max_length, tokenizer, at
 def home():
     return render_template('index.html')
 
-@app.route('/predictweb', methods=['POST'])
-def predictweb():
-    '''
-    For rendering results on HTML GUI
-    '''
-    file = request.files['image']
-    filename = os.path.join('./uploads', file.filename)
-    file.save(filename)
-    result, attention_plot, predictions = evaluate_V1(filename, loaded_encoder, loaded_decoder, max_length=20, tokenizer=tokenizer, attention_features_shape=64)
-    pred_caption = ' '.join(result)   
-    
-    return render_template('index.html', prediction_text=':{}'.format(pred_caption))
-   
 
-@app.route('/get_example', methods=['GET'])
-def get_example():
-    '''
-    Example GET method
-    '''
-    return jsonify(message="This is an example GET response")
 
-@app.route('/post_example', methods=['POST'])
-def post_example():
-    '''
-    Example POST method
-    '''
-    data = request.json
-    return jsonify(message="This is an example POST response", received_data=data)
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
+
     file = request.files['file']
     filename = os.path.join('./uploads', file.filename)
     file.save(filename)
-    result, attention_plot, predictions = evaluate_V1(filename, loaded_encoder, loaded_decoder, max_length=20, tokenizer=tokenizer, attention_features_shape=64)
-    pred_caption = ' '.join(result)   
+    # image = Image.open(io.BytesIO(file.read())).convert('RGB')
+    encoder, decoder = load_models()
+    max_length = 20  # Adjust this as per your model
+    attention_features_shape = 64  # Adjust this as per your model
+    tokenizer_path = './FinalModels/tokenizer.pkl'
+    with open(tokenizer_path, 'rb') as tokenizer_file:
+        tokenizer = pickle.load(tokenizer_file)
+    result, attention_plot, predictions = evaluate_V1(filename, encoder, decoder, max_length, tokenizer, attention_features_shape)
+    # result, attention_plot, predictions = evaluate_V1(filename, loaded_encoder, loaded_decoder, max_length=20, tokenizer=tokenizer, attention_features_shape=64)
+    # result = ' '.join([word for word in result if word not in ('<start>', '<end>')])  
     return jsonify({'caption': ' '.join(result)})
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
